7713fdb55efb4b6bc54c648a32d7cc38d9f8e391
Juan Izaguirre <juanfilpz@gmail.com>
2020-10-09 15:11:49 -0600

    recuperacion mediante correo completada

--- reset_password.php
+++ reset_password.php
@@ -26,12 +26,10 @@ if ($_POST != null) {
         $errors = 'Lamentablemente las contraseñas ingresadas no coinciden, Intentalo de Nuevo';
         $type = 'danger';
     } else {
-        $errors = 'Muy Bien Contraseña actualizada correctamente';
+        $errors = 'Muy Bien Contraseña actualizada correctamente, por favor espere unos segundos';
         $type = 'success';
         $confPass = hashPassword($pass);
         updPass($confPass, $userid);
-        //header('/index.php');
-        header("Location: /tw/", TRUE, 301);
     }
 }
 
@@ -65,7 +63,12 @@ if ($_POST != null) {
 
     <div class="container">
         <img style="margin-top:5%" src="images\tecniwahs_logo.png" alt="tecniwash logo" srcset="">
-        <?php echo showMessage($errors, $type);
+        <?php
+        if ($errors != '') {
+            echo showMessage($errors, $type);
+            // header("Location: /tw/", TRUE, 301);
+            header("Refresh:5; url=http://localhost/tw/", true, 303);
+        }
         ?>
         <div class="w3ls-login">
 
@@ -96,56 +99,6 @@ if ($_POST != null) {
             </form>
 
         </div>
-        <!--  <div id="loginbox" style="margin-top:50px;" class="mainbox col-md-6 col-md-offset-3 col-sm-8 col-sm-offset-2">
-            <div class="panel panel-info">
-                <div class="panel-heading">
-                    <div class="panel-title">Cambiar Password</div>
-                    <div style="float:right; font-size: 80%; position: relative; top:-10px"><a href="index.php">Iniciar Sesi&oacute;n</a></div>
-                </div>
-
-                <div style="padding-top:30px" class="panel-body">
-
-                    <form id="loginform" class="form-horizontal" role="form" action="guarda_pass.php" method="POST" autocomplete="off">
-
-                        <input type="hidden" id="userid" name="userid" value="<?php echo $userid; ?>" />
-
-                        <input type="hidden" id="token" name="token" value="<?php echo $token; ?>" />
-
-                        <div class="form-group">
-                            <label for="password" class="col-md-3 control-label">Nuevo Password</label>
-                            <div class="col-md-9">
-                                <div class="input-group">
-                                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                                    <input title="Nueva Contraseña" maxlength="15" type="password" class="form-control" name="password" placeholder="Password" required>
-                                    <span id="show-hide-passwd" action="hide" class="input-group-addon glyphicon glyphicon glyphicon-eye-open"></span>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div class="form-group">
-                            <label for="con_password" class="col-md-3 control-label">Confirmar Password</label>
-                            <div class="col-md-9">
-                                <div class="input-group">
-                                    <span class="input-group-addon"><i class="glyphicon glyphicon-lock"></i></span>
-                                    <input title="Confirmacion de Contraseña" maxlength="15" type="password" class="form-control" name="con_password" placeholder="Confirmar Password" required>
-                                    <span id="show-hide-passwd1" action="hide" class="input-group-addon glyphicon glyphicon glyphicon-eye-open"></span>
-                                </div>
-                            </div>
-                        </div>
-
-                        <div style="margin-top:10px" class="form-group">
-
-                            <center>
-                                <div class="col-sm-12 controls">
-                                    <button title="Modificar Contraseña" id="btn-login" type="submit" class="btn btn-success">Modificar</a>
-                                </div>
-                            </center>
-                        </div>
-                    </form>
-                    <?php echo resultBlock($errors); ?>
-                </div>
-            </div>
-        </div> -->
     </div>
 </body>
 
