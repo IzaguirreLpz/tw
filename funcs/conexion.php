46a3a290ba1936b4c8e2caa103455dbd7dd62337
JuanK Lopez <juanfilpz@gmail.com>
2020-10-11 21:24:34 -0600

    conectar a una base de datos remota

--- funcs/conexion.php
+++ funcs/conexion.php
@@ -1,7 +1,7 @@
 <?php
 
 //servidor, usuario de base de datos, contraseña del usuario, nombre de base de datos
-$mysqli = new mysqli("127.0.0.1", "root", "", "bd_ber");
+$mysqli = new mysqli("13.59.202.139", "root", "Atlashome2018_", "bd_ber");
 if (mysqli_connect_errno()) {
 	echo 'Conexion Fallida : ', mysqli_connect_error();
 	exit();
