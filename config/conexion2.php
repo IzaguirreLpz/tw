71c537694a7aa43160cfe0e7b30accb41d3b24a9
Juan Izaguirre <juanfilpz@gmail.com>
2020-11-07 21:36:36 -0600

    conexiones

--- config/conexion2.php
+++ config/conexion2.php
@@ -1,11 +1,11 @@
 <?php
-
+/* 
   $mysqli = new mysqli("testingdb.c2lgpurhnyvd.us-east-1.rds.amazonaws.com", "root", "NiOlUT2X3M1D2maxbCWI", "bd_tw");
 if (mysqli_connect_errno()) {
 	echo 'Conexion Fallida : ', mysqli_connect_error();
 	exit();
-}  
-/*
+}   */
+
 $servername = '127.0.0.1';
 $username = "root";
 $password = "";
@@ -19,5 +19,5 @@ $conn = new mysqli($servername, $username, $password, $dbname);
 if ($conn->connect_error) {
     die("Connection failed: " . $conn->connect_error);
 } 
-*/
+
 ?>
